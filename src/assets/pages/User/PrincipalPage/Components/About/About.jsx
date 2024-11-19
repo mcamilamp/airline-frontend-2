5e5d7ad386fb5a7c75940629e24e3d82548e6d15
mcamilamp <camimerpa@gmail.com>
2024-11-19 14:51:26 -0500

    about check

--- src/assets/pages/User/PrincipalPage/Components/About/About.jsx
+++ src/assets/pages/User/PrincipalPage/Components/About/About.jsx
@@ -4,6 +4,8 @@ import { GrMoney } from "react-icons/gr";
 import { BiWorld } from "react-icons/bi";
 import { FaHandHoldingHeart } from "react-icons/fa";
 
+import video from "../../../../../video/video.mp4";
+
 function About() {
   return (
     <section className="about section">
@@ -27,6 +29,26 @@ function About() {
             <FaHandHoldingHeart className="icon" />
 
             <p>Commitment to your comfort and safety.</p>
+          </div>
+        </div>
+
+        <div className="videoContainer">
+          <div className="videoCard">
+            <video src={video} loop autoPlay muted></video>
+            {/* <h3>Live the adventure.</h3> */}
+          </div>
+
+          <div className="content">
+            <h3>
+              Discover the world with us: your next adventure begins on board.
+            </h3>
+            <p>
+              Explore unique destinations with comfort, safety and the best
+              service. Your ideal trip is just a flight away!
+            </p>
+            <button className="btn">
+              <a href="#">Explore</a>
+            </button>
           </div>
         </div>
       </div>
