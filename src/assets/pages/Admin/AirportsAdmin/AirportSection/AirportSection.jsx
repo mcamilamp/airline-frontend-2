6b1fbf25e00df25f9cd3bf4e50d71dcc186433db
Camilo Benavides <benavidescamilo199@gmail.com>
2024-11-26 16:44:01 -0500

    Adding Crud on Airports

--- src/assets/pages/Admin/AirportsAdmin/AirportSection/AirportSection.jsx
+++ src/assets/pages/Admin/AirportsAdmin/AirportSection/AirportSection.jsx
@@ -1,8 +1,92 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { Link } from "react-router-dom";
 import "../AirportSection/AirportSection.scss";
+import { Requests } from "../../../Login/components/scripts/requests";
+
+const req = new Requests();
+
+async function addAirport(airport) {
+  return req.api.post("/airports", airport).then((res) => res.data)
+}
+
+async function getAllAirports() {
+  return req.api.get("/airports").then((res) => res.data)
+}
+
+async function deleteAirport(id) {
+  return req.api.delete("/airports/" + id).then((res) => res.data)
+}
+
+function airportMapper(airport) {
+  return {
+    id: airport.idAirport,
+    name: airport.name,
+    city: airport.city,
+    country: airport.country,
+    code: airport.iataCode,
+  }
+}
 
 function AirportSection() {
+  const [airports, setAirports] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const result = await getAllAirports();
+        console.log(result)
+        setAirports(result.map(airportMapper));
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+        newAirport.name &&
+        newAirport.city &&
+        newAirport.country &&
+        newAirport.code
+    ) {
+      const body = { ...newAirport };
+      console.log(body);
+
+      addAirport(body)
+          .then((airport) => getAllAirports())
+          .then((airports) => {
+            setAirports(airports.map(airportMapper));
+            setNewAirport({
+              city: "",
+              country: "",
+              name: "",
+            });
+          });
+    } else {
+      alert("Please fill out all fields");
+    }
+  };
+
+  const handleDelete = (id) => {
+    deleteAirport(id)
+        .then(() => getAllAirports())
+        .then((airports) => setAirports(airports));
+  }
+
+  const [newAirport, setNewAirport] = useState({
+    city: "",
+    country: "",
+    name: "",
+  });
+  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewAirport({ ...newAirport, [name]: value });
+  };
+
+
   return (
     <section className="airport-section">
       <aside className="sidebar">
@@ -39,37 +123,58 @@ function AirportSection() {
       <div className="airport-grid">
         <div className="airport-info">
           <h2>Current Airports</h2>
-          <div className="info-a">
-            <ul>
-              <li>
-                <p>Id Airport: </p>
-                <p>City: </p>
-                <p>Country: </p>
-                <p>Name: </p>
-                <button href="#" className="btnDelete">
-                  Delete Airport
-                </button>
-              </li>
-            </ul>
-          </div>
+          {airports.length > 0 ? (
+              <ul>
+                {airports.map((ap) => (
+                    <div className="info-a">
+                      <ul>
+                        <li>
+                          <p>Id Airport: {ap.id}</p>
+                          <p>Name: {ap.name}</p>
+                          <p>City: {ap.city}</p>
+                          <p>Country: {ap.country}</p>
+                          <p>Code: {ap.code}</p>
+                          <button href="#" className="btnDelete" onClick={() => handleDelete(ap.id)}>
+                            Delete Airport
+                          </button>
+                        </li>
+                      </ul>
+                    </div>
+                ))}
+              </ul>
+          ) : (
+              <p>No destinations available</p>
+          )}
         </div>
         <div className="airport-form">
           <h2>Add new Airport</h2>
-          <form action="">
-            <label htmlFor="">
+          <form action="" onSubmit={handleSubmit}>
+            {/*<label htmlFor="">
               Id Airport: <input type="text" />
+            </label>*/}
+
+            <label htmlFor="">
+              Name: <input type="text"
+                           onChange={handleChange}
+                           name="name"/>
             </label>
 
             <label htmlFor="">
-              City: <input type="text" />
+              City: <input type="text"
+                           onChange={handleChange}
+                           name="city"/>
             </label>
 
             <label htmlFor="">
-              Country: <input type="text" />
+              Country: <input type="text"
+                              onChange={handleChange}
+                              name="country"/>
             </label>
 
             <label htmlFor="">
-              Name: <input type="text" />
+              Code: <input type="text"
+                           onChange={handleChange}
+                           name="code"/>
             </label>
 
             <button type="submit" className="btn">
